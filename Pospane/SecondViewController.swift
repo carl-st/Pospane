cd9a9da7d5276502373d42ba6feb6841b1ea930b
Karol Stępień <kstepien@future-processing.com>
2018-03-12 21:55:21 +0100

    WatchKit project init

---
+++ Pospane/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Pospane
+//
+//  Created by Karol Stępień on 12.03.2018.
+//  Copyright © 2018 Carste IT. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

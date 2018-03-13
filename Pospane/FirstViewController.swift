76aec963d6e4a53682ced2e9fae84049b939e348
Karol Stępień <kstepien@future-processing.com>
2018-03-13 23:24:14 +0100

    Get heart rate

--- Pospane/FirstViewController.swift
+++ Pospane/FirstViewController.swift
@@ -7,19 +7,104 @@
 //
 
 import UIKit
+import HealthKit
+import WatchConnectivity
 
 class FirstViewController: UIViewController {
-
+    private let healthStore = HKHealthStore()
+    private var observerQuery: HKObserverQuery?
+    private let heartRateUnit = HKUnit(from: "count/min")
+    private let session: WCSession? = WCSession.isSupported() ? WCSession.default : nil
+    
+    var validSession: WCSession? {
+        
+        // Adapted from https://gist.github.com/NatashaTheRobot/6bcbe79afd7e9572edf6
+        
+        #if os(iOS)
+            if let session = session, session.isPaired && session.isWatchAppInstalled {
+                return session
+            }
+        #elseif os(watchOS)
+            return session
+        #endif
+        return nil
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        authorizeHealthKit()
+    }
+    
+    func updateHeartRate(heartRateValue: Double) {
+        print("Heart rate value: \(heartRateValue)")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func authorizeHealthKit() {
+        let healthKitTypes: Set = [
+            // access step count
+            HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!
+        ]
+
+        healthStore.requestAuthorization(toShare: healthKitTypes, read: healthKitTypes) { (bool, error) in
+            if let e = error {
+                print("oops something went wrong during authorisation \(e.localizedDescription)")
+            } else {
+                print("User has completed the authorization flow")
+            }
+        }
     }
-
-
+    
+    func observerHeartRateSamples() {
+        let heartRateSampleType = HKObjectType.quantityType(forIdentifier: .heartRate)
+        
+        if let observerQuery = observerQuery {
+            healthStore.stop(observerQuery)
+        }
+        
+        observerQuery = HKObserverQuery(sampleType: heartRateSampleType!, predicate: nil) { (_, _, error) in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            
+            self.fetchLatestHeartRateSample { (sample) in
+                guard let sample = sample else {
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    let heartRate = sample.quantity.doubleValue(for: self.heartRateUnit)
+                    print("Heart Rate Sample: \(heartRate)")
+                    self.updateHeartRate(heartRateValue: heartRate)
+                }
+            }
+        }
+        
+        healthStore.execute(observerQuery!)
+    }
+    
+    func fetchLatestHeartRateSample(completionHandler: @escaping (_ sample: HKQuantitySample?) -> Void) {
+        guard let sampleType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate) else {
+            completionHandler(nil)
+            return
+        }
+        
+        let predicate = HKQuery.predicateForSamples(withStart: Date.distantPast, end: Date(), options: .strictEndDate)
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
+        let query = HKSampleQuery(sampleType: sampleType,
+                                  predicate: predicate,
+                                  limit: Int(HKObjectQueryNoLimit),
+                                  sortDescriptors: [sortDescriptor]) { (_, results, error) in
+                                    if let error = error {
+                                        print("Error: \(error.localizedDescription)")
+                                        return
+                                    }
+                                    
+                                    completionHandler(results?[0] as? HKQuantitySample)
+        }
+        
+        healthStore.execute(query)
+    }
+    
 }
 

aa54f2e480ffbbcb96e29db1ae670b6ab4e0f638
Karol Stępień <kstepien@future-processing.com>
2018-03-20 22:35:08 +0100

    basic sleep analyzer, swiftlint, pod init

---
+++ Podfile
@@ -0,0 +1,38 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, ' 11.0'
+
+target 'Pospane' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Pospane
+  pod 'SwiftLint'
+
+  target 'PospaneTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'PospaneUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
+
+target 'PospaneWatch' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PospaneWatch
+  pod 'SwiftLint'
+
+end
+
+target 'PospaneWatch Extension' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PospaneWatch Extension
+
+end
--- Pospane/FirstViewController.swift
+++ Pospane/FirstViewController.swift
@@ -15,37 +15,26 @@ class FirstViewController: UIViewController {
     private var observerQuery: HKObserverQuery?
     private let heartRateUnit = HKUnit(from: "count/min")
     private let session: WCSession? = WCSession.isSupported() ? WCSession.default : nil
-    
-    var validSession: WCSession? {
-        
-        // Adapted from https://gist.github.com/NatashaTheRobot/6bcbe79afd7e9572edf6
-        
-        #if os(iOS)
-            if let session = session, session.isPaired && session.isWatchAppInstalled {
-                return session
-            }
-        #elseif os(watchOS)
-            return session
-        #endif
-        return nil
-    }
-    
+    var endTime: Date!
+    var alarmTime: Date!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         authorizeHealthKit()
     }
-    
-    func updateHeartRate(heartRateValue: Double) {
-        print("Heart rate value: \(heartRateValue)")
-    }
 
     private func authorizeHealthKit() {
-        let healthKitTypes: Set = [
-            // access step count
-            HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!
+        let typesToRead: Set = [
+            HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!,
+            HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!
         ]
+        
+        let typesToShare: Set = [
+            HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!
+        ]
+        
 
-        healthStore.requestAuthorization(toShare: healthKitTypes, read: healthKitTypes) { (bool, error) in
+        healthStore.requestAuthorization(toShare: typesToShare, read: typesToRead) { (bool, error) in
             if let e = error {
                 print("oops something went wrong during authorisation \(e.localizedDescription)")
             } else {
@@ -54,57 +43,70 @@ class FirstViewController: UIViewController {
         }
     }
     
-    func observerHeartRateSamples() {
-        let heartRateSampleType = HKObjectType.quantityType(forIdentifier: .heartRate)
+    func saveSleepAnalysis() {
         
-        if let observerQuery = observerQuery {
-            healthStore.stop(observerQuery)
-        }
-        
-        observerQuery = HKObserverQuery(sampleType: heartRateSampleType!, predicate: nil) { (_, _, error) in
-            if let error = error {
-                print("Error: \(error.localizedDescription)")
-                return
-            }
-            
-            self.fetchLatestHeartRateSample { (sample) in
-                guard let sample = sample else {
+
+        if let sleepType = HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis) {
+
+            let object = HKCategorySample(type:sleepType, value: HKCategoryValueSleepAnalysis.inBed.rawValue, start: self.alarmTime, end: self.endTime)
+
+            healthStore.save(object, withCompletion: { (success, error) -> Void in
+                
+                if error != nil {
                     return
                 }
                 
-                DispatchQueue.main.async {
-                    let heartRate = sample.quantity.doubleValue(for: self.heartRateUnit)
-                    print("Heart Rate Sample: \(heartRate)")
-                    self.updateHeartRate(heartRateValue: heartRate)
+                if success {
+                    print("My new data was saved in HealthKit")
+                    
+                } else {
+ 
+                }
+            })
+
+            let object2 = HKCategorySample(type:sleepType, value: HKCategoryValueSleepAnalysis.asleep.rawValue, start: self.alarmTime, end: self.endTime)
+
+            healthStore.save(object2, withCompletion: { (success, error) -> Void in
+                if error != nil {
+                    return
+                }
+                
+                if success {
+                    print("My new data (2) was saved in HealthKit")
+                } else {
+
+                }
+            })
+        }
+    }
+    
+    func retrieveSleepAnalysis() {
+        
+        if let sleepType = HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis) {
+            
+            let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+            
+            let query = HKSampleQuery(sampleType: sleepType, predicate: nil, limit: 30, sortDescriptors: [sortDescriptor]) { (query, tmpResult, error) -> Void in
+                
+                if error != nil {
+                    
+                    return
+                    
+                }
+                
+                if let result = tmpResult {
+                    
+                    for item in result {
+                        if let sample = item as? HKCategorySample {
+                            let value = (sample.value == HKCategoryValueSleepAnalysis.inBed.rawValue) ? "InBed" : "Asleep"
+                            print("Healthkit sleep: \(sample.startDate) \(sample.endDate) - value: \(value)")
+                        }
+                    }
                 }
             }
+            
+            healthStore.execute(query)
         }
-        
-        healthStore.execute(observerQuery!)
     }
-    
-    func fetchLatestHeartRateSample(completionHandler: @escaping (_ sample: HKQuantitySample?) -> Void) {
-        guard let sampleType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate) else {
-            completionHandler(nil)
-            return
-        }
-        
-        let predicate = HKQuery.predicateForSamples(withStart: Date.distantPast, end: Date(), options: .strictEndDate)
-        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
-        let query = HKSampleQuery(sampleType: sampleType,
-                                  predicate: predicate,
-                                  limit: Int(HKObjectQueryNoLimit),
-                                  sortDescriptors: [sortDescriptor]) { (_, results, error) in
-                                    if let error = error {
-                                        print("Error: \(error.localizedDescription)")
-                                        return
-                                    }
-                                    
-                                    completionHandler(results?[0] as? HKQuantitySample)
-        }
-        
-        healthStore.execute(query)
-    }
-    
 }
 
